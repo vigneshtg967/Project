cc2cd50183c5f119bc42f105a8531bdb910a2e0b
MManoj915 <49112489+MManoj915@users.noreply.github.com>
2024-03-02 11:31:52 +0530

    Update HomeController.cs

--- Controller/HomeController.cs
+++ Controller/HomeController.cs
@@ -6,10 +6,30 @@ namespace Project.Controller
     {
         public IActionResult Index()
         {
-            int x = 5;
-            int y = 6;
-            int sum = x + y;
-            Console.WriteLine(sum);
+            // Prompting the user to enter the first number
+        Console.Write("Enter a number: ");
+        // Reading the first number entered by the user and converting it to an integer
+        int num1 = Convert.ToInt32(Console.ReadLine());
+
+        // Prompting the user to enter the second number
+        Console.Write("Enter another number: ");
+        // Reading the second number entered by the user and converting it to an integer
+        int num2 = Convert.ToInt32(Console.ReadLine());
+
+        // Displaying addition of the two numbers
+        Console.WriteLine("{0} + {1} = {2}", num1, num2, num1 + num2);
+
+        // Displaying subtraction of the two numbers
+        Console.WriteLine("{0} - {1} = {2}", num1, num2, num1 - num2);
+
+        // Displaying multiplication of the two numbers
+        Console.WriteLine("{0} x {1} = {2}", num1, num2, num1 * num2);
+
+        // Displaying division of the two numbers
+        Console.WriteLine("{0} / {1} = {2}", num1, num2, num1 / num2);
+
+        // Displaying modulus (remainder) of the two numbers
+        Console.WriteLine("{0} mod {1} = {2}", num1, num2, num1 % num2);
             return View();
         }
     }
